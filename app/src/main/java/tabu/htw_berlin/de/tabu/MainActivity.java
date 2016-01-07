497a504cad8f46e9f7f0f82137e39d992ef933ca
Philipp Lange <philipp.lange23@gmail.com>
2016-01-07 18:41:41 +0100

    Regel-Button auf der MainActivity funktionstüchtig gemacht

--- app/src/main/java/tabu/htw_berlin/de/tabu/MainActivity.java
+++ app/src/main/java/tabu/htw_berlin/de/tabu/MainActivity.java
@@ -1,13 +1,47 @@
 package tabu.htw_berlin.de.tabu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnStarteSpiel;
+    Button btnRegeln;
+    Button btnBeenden;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Erzeugen der Widgets
+        btnStarteSpiel = (Button) findViewById(R.id.btn_starte_spiel);
+        btnRegeln      = (Button) findViewById(R.id.btn_regeln);
+        btnBeenden     = (Button) findViewById(R.id.btn_beenden);
+
+        // Buttons dem Listener vorstellen
+        btnStarteSpiel.setOnClickListener(this);
+        btnRegeln.setOnClickListener(this);
+        btnBeenden.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_starte_spiel:
+                break;
+            case R.id.btn_regeln:
+                starteRegelnActivity();
+                break;
+            case R.id.btn_beenden:
+                break;
+        }
+    }
+
+    private void starteRegelnActivity() {
+        Intent intent = new Intent(this, RegelnActivity.class);
+        startActivity(intent);
     }
 }
